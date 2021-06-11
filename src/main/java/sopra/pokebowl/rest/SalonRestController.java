dab1101721a4222b1f2916ac65bda614e99e20cf
tristan <tris@g>
2021-06-11 14:21:31 +0200

    modif test unit

--- src/main/java/sopra/pokebowl/rest/SalonRestController.java
+++ src/main/java/sopra/pokebowl/rest/SalonRestController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -76,6 +77,7 @@ public class SalonRestController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public void delete(@PathVariable Long id) {
 		salonRepo.deleteById(id);
 	}
